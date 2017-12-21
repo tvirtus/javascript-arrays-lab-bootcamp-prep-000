4aaf5b6d4c1bd19b8295d4ff47afac700992d7b8
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-21 20:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.lenght - 1);
+  return kittens.slice(0, kittens.lenght - 2);
 }
 
 function removeFirstKitten() {
